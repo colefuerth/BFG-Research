a9fb46afc0c4629433a81047c6df21254f0cba11
SquareWheelBike <colefuerth@gmail.com>
2022-03-04 15:37:34 -0500

    Devices are now more modular

--- src/Devices.py
+++ src/Devices.py
@@ -8,15 +8,16 @@ class HID:
         self.name = "HID"
         self.manufacturer = "Unknown"
 
+    def __str__(self):
+        return f'{self.name} ({self.manufacturer})'
+
 class BFG(HID):
     # Generic class to interface with BFG devices
     # this will provide a universal interface for data collecton between the three different BFG devices
-    def __init__(self):
-        self.name = 'generic BFG device'
+    def __init__(self, i2c_address):
+        super().__init__(i2c_address)
+        self.name = 'BFG device'
         self.manufacturer = 'generic'
-        
-    def __str__(self):
-        return self.name + ' (' + self.serial + ')'
 
     def percent_capacity(self):
         pass
@@ -35,18 +36,24 @@ class BFG(HID):
 
 class BFG_1(BFG):
     # Specific class for BFG-1
-    def __init__(self):
-        self.name = 'LC7009203F'
-        self.manufacturer = 'ON Semiconductor'
+    def __init__(self, i2c_address):
+        super().__init__(i2c_address)
+        # product page: https://www.adafruit.com/product/4712
+        # library: https://github.com/adafruit/Adafruit_CircuitPython_LC709203F
+        # notes: you will need to slow down the i2c port
+        self.name = 'LC709203F'
+        self.manufacturer = 'AdaFruit'
 
 class BFG_2(BFG):
     # Specific class for BFG-2
-    def __init__(self):
+    def __init__(self, i2c_address):
+        super().__init__(i2c_address)
         self.name = 'LTC2941'
         self.manufacturer = 'Analog Device'
 
 class BFG_3(BFG):
     # Specific class for BFG-3
-    def __init__(self):
+    def __init__(self, i2c_address):
+        super().__init__(i2c_address)
         self.name = 'MAX 17043'
         self.manufacturer = 'Maxim Integrated'
