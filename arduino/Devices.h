326cbae320e0f7ddf1afac3ecde8ce36ec4d1a9f
SquareWheelBike <colefuerth@gmail.com>
2022-03-09 02:08:10 -0500

    arduino-cli implemented in makefile

--- arduino/Devices.h
+++ arduino/Devices.h
@@ -1,11 +1,18 @@
 // Header file for BFG implementations
 
-#include <Arduino.h>
+#include <Arduino.h>    // String implemented here
+// #include <Wire.h> // for I2C
+#include <Adafruit_LC709203F.h>
 
-// generic class for BFG implementations
+/**
+ * generic class for BFG implementations
+ * 
+ * All hardware devices will inherit from this superclass. 
+ * This way, all payloads can be handled in a generic way, with one universal serial interface. The pi at the other end can worry about valid calls and such.
+ */
 class Device {
 public:
-    Device() {this->_D = "none"; this->_addr = 0;}
+    Device() {this->_D = "none";}
     void begin();                   // start I2C interface
     String D() {return this->_D;}    // Device ID
     float V() {return 0;}           // Voltage (V)
@@ -15,7 +22,12 @@ public:
     float P() {return 0;}           // Battery percentage
     float T() {return 0;}           // Temperature (C)
     float W() {return 0;}           // Wattage (W)
-private:
+protected:
     String _D;                      // Device ID
-    int _addr;                      // I2C address
 };
+
+
+// class LC709203F : public Device {
+// public:
+//     LC709203F(Adafruit_LC709203F *lc) : Device() {this->_D = "LC709203F"; this->_lc = lc; this->lc.begin()}
+// };
