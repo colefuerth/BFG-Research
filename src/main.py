f7f4b8252c68a4c44d97e7f2c04ea43a51afd2bf
Cole Fuerth <colefuerth@gmail.com>
2022-03-22 23:21:28 -0400

    async ready for testing

--- src/main.py
+++ src/main.py
@@ -21,13 +21,33 @@ def main():
 
     header = ['Device', *[attributes[k] for k in s.updstr], 'Timestamp']
     print(','.join(header))
-    sendpayload(sensors)
-    c = count(0)
-    while next(c) < len(sensors):
-        # need to recv packet and then process it
-        print(','.join([s.name, *s.data(), datetime.now(timezone.utc).__str__()]))
-        # writer.writerow(s.data('VICPTHW'))
-        time.sleep(1/hz)
+
+    while(True):
+        # send a request packet with all sensors in it
+        sendpayload(sensors)
+
+        # record last time sent, so we know when to send a new packet
+        last_time = time.time()
+
+        # wait for all response packets to be received
+        responses = set()
+        while responses != set(sensors.keys()):
+            # need to recv packet and then process it
+            recv = recvpayload()
+            if recv:
+                # update the sensor
+                s = revc['D']
+                if s == 'LOG':
+                    print('LOG: ' + recv['M'])
+                    continue
+                c.add(s)
+                sensors[s].update(recv)
+                print(','.join([s.name, *sensors[s].data(), datetime.now(timezone.utc).__str__()]))
+            time.sleep(5/1000)
+            
+        # wait until the next packet should be sent
+        if (time.time() - last_time) < 1/hz:
+            time.sleep(1/hz - (time.time() - last_time))
 
 
 if __name__ == '__main__':
