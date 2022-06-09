96d46db93b214ccac8c9252e54d5013a9cc6662b
Cole Fuerth <fuert11g@uwindsor.ca>
2022-06-09 18:16:53 -0400

    automatic COM port detection added

---
+++ src/ComPort.py
@@ -0,0 +1,27 @@
+# A utility for determining the COM port of the Arduino, using arduino-cli
+
+import subprocess as sp
+import platform
+
+if platform.system() == 'Windows':
+    arduino_cli = 'arduino-cli/win64/arduino-cli.exe'
+elif platform.system() == 'Linux':
+    arduino_cli = 'arduino-cli/linux64/arduino-cli'
+else:
+    raise Exception('Unsupported OS')
+
+# get the list of boards from `arduino-cli board list`
+
+
+def get_arduino_comport():
+    # return sp.check_output([arduino_cli, 'board', 'list']).decode('utf-8').split('\n')
+    for i, line in enumerate(sp.check_output([arduino_cli, 'board', 'list']).decode('utf-8').strip().split('\n')[1:]):
+        # if line contains 'Arduino', return the COM port
+        if 'Arduino' in line:
+            for token in line.split(' '):
+                if token.startswith('COM'):
+                    return token.strip()
+
+
+if __name__ == '__main__':
+    print(get_arduino_boards())
